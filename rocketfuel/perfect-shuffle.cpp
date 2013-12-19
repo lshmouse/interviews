d3c79cc8730af52728090465f3992a6bf7e252ce
liushaohui <lshmouse@gmail.com>
2013-12-19 14:45:06 +0800

    add perfect shuffle and sliding-window-maximum problems

---
+++ rocketfuel/perfect-shuffle.cpp
@@ -0,0 +1,5 @@
+/**
+ * Perfect shuffle
+ * Questions: [a1, a2, a3, a4, b1, b2, b3, b4] 
+ *          ->[a1, b1, a2, b2, a3, b3, a4, b4] inplace
+ */
---
+++ rocketfuel/sliding-window-maximum.cpp
@@ -0,0 +1,8 @@
+/**
+ * http://leetcode.com/2011/01/sliding-window-maximum.html
+ * Questions: You are given an array of size n. There is a sliding window of size k(<n).
+ * Return an array containing max element in each window position(from leftmost window position to rightmost).
+ * Eg.
+ * input: A=4,7,3,6,8,2,4,3,2,5,4 k=4
+ * output:B=7,8,8,8,8,4,5,5
+ */
